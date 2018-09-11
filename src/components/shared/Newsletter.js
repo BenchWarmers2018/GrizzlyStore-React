401595354a84df93d11ca86796eb6ee994fd31f5
Quach <723313@1000610001854M.local>
2018-09-12 09:11:40 +1000

    working on random facts random generator from json file

--- src/components/shared/Newsletter.js
+++ src/components/shared/Newsletter.js
@@ -1,63 +1,99 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import bearFacts from './bearFacts.json';
+// import {getFact} from "../../../public/assets/js/facts";
+
 
 class Newsletter extends Component {
 
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
-        this.state = { random: 0 };
+        this.state = {
+            random: 0,
+            randomFacts: bearFacts,
+        };
     }
 
-    handleClick () {
+    // componentDidMount() {
+    //     fetch('./bearFacts.json')
+    //         .then(response => response.json())
+    //         .then(randomFacts => this.setState({ randomFacts }));
+    //
+    //
+    //     // fetch('./bearFacts.json')
+    //     //     .then(results => {
+    //     //         let
+    //     //
+    //     //     })
+    //
+    // }
 
-        const facts = [
-            "Panda",
-            "Koala",
-            "Polar",
-            "Hi",
-            "My",
-            "Name",
-            "Is",
-            "Julia"
-        ];
 
-        const randomNumber = Math.floor(Math.random()*facts.length);
-        this.setState({ random:facts[randomNumber] });
+
+
+    handleClick() {
+
+
+        // let parsed = JSON.parse(bearFacts);
+
+        let bearFactsArray = [];
+
+        for(let x in bearFacts.Facts){
+            bearFactsArray.push(bearFacts.Facts[x]);
+        }
+
+        // let bearFactConvert = data.map((value, index) => {
+        //     return (
+        //         <div key={index}>
+        //             {Object.keys(value)[0]}
+        //         </div>
+        //     );
+        // });
+        //
+        // let pairs = [];
+        // for(let key in this.props.data){
+        //     pairs.push({key});
+        // }
+
+        // let bearFactArray = [];
+        // for (let i in bearFacts.Facts) {
+        //     bearFactArray.push(bearFacts.Facts[i].);
+        // }
+        console.log(bearFactsArray[1]);
+
+
+        // const randomNumber = Math.floor(Math.random() * bearFacts.Facts.length);
+        // console.log(randomNumber);
+        // console.log(bearFactArray.length);
+        //
+        // this.setState({random: bearFactArray[randomNumber].fact});
     }
 
     render() {
-        return(
-                <div className="newsletter">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col">
-                                <div className="newsletter_border"></div>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-lg-8 offset-lg-2">
-                                <div className="newsletter_content text-center">
-                                    <div className="newsletter_title">Bear Facts: &hearts;</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="container">
-                            <div className="row flex-center">
-                                <button id="button" className="btn-large new-quote-button"  onClick={this.handleClick.bind(this)}>
-                                    New Quote
-                                </button>
-                            </div>
-                            <div className="row flex-center">
-                                <main className="card">
-                                    <p className="quote card-body center" id="bearFactSection">{this.state.random}</p>
-                                </main>
-                            </div>
-
+        return (
+            <div className="newsletter">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="newsletter_border"></div>
                         </div>
                     </div>
+
+
+                    <div className="container">
+                        <div className="row flex-center">
+                            <main className="card random_fact_div">
+                                <div className="newsletter_title random_fact_title"
+                                     onClick={this.handleClick.bind(this)}>Bear Trivia
+                                </div>
+                                <p className="quote card-body center" id="bearFactSection"
+                                   onClick={this.handleClick.bind(this)}>{this.state.random}</p>
+                            </main>
+                        </div>
+
+                    </div>
                 </div>
+            </div>
         );
     }
 }
