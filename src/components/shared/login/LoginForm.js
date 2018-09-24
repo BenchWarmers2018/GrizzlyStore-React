0ee5e2d2c0ed6f0297e949f4212a8b9435720b97
a-nthonymai <Anthony.Mai@hotmail.com>
2018-09-25 09:51:51 +1000

    Validation implemented

--- src/components/shared/login/LoginForm.js
+++ src/components/shared/login/LoginForm.js
@@ -14,16 +14,26 @@ const App = props => {
 
   return(
     <Form>
-      <div>
-        <label className="image-replace email" htmlFor="emailAddress">Email Address</label>
-        {touched.emailAddress && errors.emailAddress && <p>{errors.emailAddress}</p>}
-        <Field className="full-width has-padding has-border" name="emailAddress" type="email" placeholder="E-mail" value={values.emailAddress} onChange={handleChange}/>
-      </div>
-      <div>
-        <label className="image-replace password" htmlFor="password">Password</label>
-        {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field className="full-width has-padding has-border" name="password" type="password"  placeholder="Password" values={values.password} onChange={handleChange}/>
-      </div>
+      <form className = "form">
+        {/* Email Field */}
+        <p className="fieldset">
+          <label className="image-replace email" htmlFor="emailAddress">Email Address</label>
+          <Field className="full-width has-padding has-border" name="emailAddress" type="email" placeholder="E-mail" value={values.emailAddress} onChange={handleChange}/>
+          {touched.emailAddress && errors.emailAddress && <p>{errors.emailAddress}</p>}
+          {/* {errors.emailAddress} */}
+        </p>
+
+        {/* Password Field */}
+        <p className="fieldset">
+          <label className="image-replace password" htmlFor="password">Password</label>
+          <Field className="full-width has-padding has-border" name="password" type="password"  placeholder="Password" values={values.password} onChange={handleChange}/>
+          {touched.password && errors.password && <span className="error-message">{errors.password}</span>}
+        </p>
+
+        <p className="fieldset">
+          <input className="full-width" type="submit" value="Login"/>
+        </p>
+      </form>
     </Form>
   )
 }
@@ -36,8 +46,8 @@ const FormikApp = withFormik({
     }
   },
   validationSchema: Yup.object().shape({
-    emailAddress: Yup.string().email('Email Address is not valid').required('Email Address is required'),
-    password: Yup.string().min(8, 'Password must be 8 characters or longer').required('Password is required')
+    emailAddress: Yup.string().email('Email Address is not valid.').required('An Email Address is required!'),
+    password: Yup.string().min(8, 'Password must be 8 characters or longer.').required('A Password is required!')
   }),
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
     setTimeout(() => {
@@ -50,13 +60,5 @@ const FormikApp = withFormik({
     }, 2000)
   }
 })(App)
-
-// const App = () => (
-//
-// )
-//
-// const FormikApp = withFormik( {
-
-// })(LoginForm)
 
 export default FormikApp;
