b9dde3541f429d81c28b23fd42d7ece87346cb84
meimein <iantanxjw@gmail.com>
2018-10-02 08:57:16 +1000

    added some configuration for google

--- src/components/shared/GoogleLogin.js
+++ src/components/shared/GoogleLogin.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import axios from 'axios';
-import {fetchGoogleAccounts} from "../../actions/googleaccountAction"
+import {fetchGoogleAccounts} from "../../actions/googleaccountAction";
 import connect from "react-redux/es/connect/connect";
+
+const firebaseAuthKey = "firebaseAuthInProgress";
 
 class GoogleLogin extends Component {
 
@@ -32,23 +34,31 @@ class GoogleLogin extends Component {
 
     }
 
+    // handleGoogleLogin() {
+    //     loginWithGoogle()
+    //         .catch(function (error) {
+    //             alert(error); // or show toast
+    //             localStorage.removeItem(firebaseAuthKey);
+    //         });
+    //     localStorage.setItem(firebaseAuthKey, "1");
+    // }
+
     componentDidMount() {
-
         firebase.auth().onAuthStateChanged(user => {
-            this.setState({
-                isSignedIn: !!user
-            })
-
-            //Creating user to pass through to backend
-            const googleUser = {idGoogleAccount: user.uid, googleAccountEmailAddress: user.email};
-
-            //Logging user to frontend console
-            console.log(user)
-
-            //Dispatching googleUser to Action file
-            this.props.dispatch(fetchGoogleAccounts(googleUser));
-        })
-}
+            if (user) {
+                //Logging user to frontend console
+                console.log("User signed in: ", JSON.stringify(user));
+                // so apparently "qa" user.qa is accessToken
+                console.log(user.qa);
+                //Creating user to pass through to backend
+                const googleUser = {idGoogleAccount: user.uid, googleAccountEmailAddress: user.email};
+                const appTokenKey = "token";
+                localStorage.setItem(appTokenKey, user.qa);
+                //Dispatching googleUser to Action file
+                this.props.dispatch(fetchGoogleAccounts(googleUser));
+            }
+        });
+    }
 
     render() {
         return (
