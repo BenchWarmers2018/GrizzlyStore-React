976473ab984448649560c39fa1ce823802d828cf
PaarthB <paarthbhasin4@gmail.com>
2018-10-02 14:15:06 +1000

    run react via Nginx

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,6 +5,7 @@ pipeline {
          steps {
             echo 'Build Stage'
             sh 'npm install'
+            sh 'npm run build'
          }
       }
       stage('Test') {
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-echo 'Deployment in progress'
-pwd
-npm start
+echo 'Creating React build files to be served by Nginx'
+
+echo 'Moving build files to Nginx Directory'
+sudo rm -rf /opt/GrizzlyStoreApp/*
+sudo cp -R /var/lib/jenkins/workspace/GrizzlyStore-React/build/* /opt/GrizzlyStoreApp/
+
+if (( $(ps -ef | grep -v grep | grep nginx | wc -l) > 0))
+	then
+		echo "Nginx is already, need to restart it!"
+		sudo systemctl stop nginx.service || true
+fi
+
+sudo systemctl start nginx.service
