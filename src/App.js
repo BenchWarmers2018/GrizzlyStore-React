f5825ea06c94cd824677b7931b2989598cc2758e
Singh <723304@1000810001142M.local>
2018-10-11 09:42:36 +1100

    Constant change

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isAdmin : true,
+            isAdmin : false,
 
             currentUser: null,
             isAuthenticated: false,
