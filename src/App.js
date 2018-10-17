a57af9dea7ac8f9f43a4aa82dfd883fc0cf53d54
Anthony Mai <Anthony.mai@hotmail.com>
2018-10-17 23:52:50 +1100

    Reverted isAdmin status back to false

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isAdmin : true,
+            isAdmin : false,
 
             currentUser: null,
             isAuthenticated: false,
