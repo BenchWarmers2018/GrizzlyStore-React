60e9712a05a5cd8da42f5c72a72de58a04ac0f24
a-nthonymai <Anthony.Mai@hotmail.com>
2018-10-17 23:48:22 +1100

    Removed more unnecessary code

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -136,7 +136,6 @@ export default function reducer(state=initialState, action) {
                 ...state,
                 items: newItems,
                 updating: false,
-                uploaded: true,
                 updated: true,
                 updateItemMessages: "Item updated successfully!",
             }
