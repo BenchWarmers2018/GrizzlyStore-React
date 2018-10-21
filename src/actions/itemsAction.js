56b059f757b09806090096efaed99b5d388a1325
a-nthonymai <Anthony.Mai@hotmail.com>
2018-10-21 21:17:20 +1100

    More restorations

--- src/actions/itemsAction.js
+++ src/actions/itemsAction.js
@@ -212,31 +212,22 @@ export function fetchPopularItems() {
 }
 
 
-export function addItem(itemName, itemDescription, itemImage, itemPrice, itemStock, itemSales, itemCategory) {
+export function addItem(formData) {
     return function (dispatch) {
-        let data = new FormData();
-        let header = {
-            'Content-Type': 'multipart/form-data'
-        };
-        let item = {
-            itemName: itemName,
-            itemPrice: itemPrice,
-            itemStockLevel: itemStock,
-            itemSalePercentage: itemSales,
-            itemDescription: itemDescription
-        };
-        data.append('file', itemImage);
-        data.append('item', JSON.stringify(item));
-        data.append('category', itemCategory);
-
-        dispatch({type: ADD_ITEM}); //
-        axios.post(URL_ITEM + "/items/addItem", data, {headers: header}).then((response) => {
-            console.log("TESTING " + response.data.entities);
-            dispatch({type: ADD_ITEM_FULFILLED, payload: response.data})
-        })
-            .catch((err) => {
-                dispatch({type: ADD_ITEM_REJECTED, payload: err})
-            })
+        dispatch({type: ADD_ITEM});
+        axios.post(URL_ITEM + "/items/addItem", formData)
+          .then((response) => {
+              dispatch({type: ADD_ITEM_FULFILLED, payload: response.data})
+          })
+          .catch((error) => {
+              if (error.message === "Network Error")
+                  dispatch({
+                      type: SERVER_NOT_FOUND,
+                      payload: 'The server is currently offline. Please try again later.'
+                  })
+              else
+                  dispatch({type: ADD_ITEM_REJECTED, payload: error.response.data.errors})
+          })
     }
 }
 
